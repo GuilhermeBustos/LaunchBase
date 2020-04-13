b4986e345ca376a389c4a0739ed72a0e086b32f2
Guilherme Bustos <guilherme.bustos@outlook.com>
2020-04-13 13:56:41 -0300

    Added operadores.js!

---
+++ operadores.js
@@ -0,0 +1,32 @@
+//DESAFIO 1
+const idade = 18
+
+// if (idade === 17) {
+//     console.log('Volte quando completar 18 anos!')
+// }
+
+// console.log(5 == 5 && 6 == 6) //true
+// console.log(5 == 5 && 6 != 6) //false
+
+// console.log(5 != 5 || 6 == 6) //true
+// console.log(5 == 5 || 6 != 6) //true
+
+// console.log(!(5 > 6)) //true
+// console.log(!(5 < 6)) //false
+
+// verficiar se a pessoa é maior igual a 18 anos
+// se sim, deixar entrar, se não, bloquear a entrada
+// se a pessoa tiver 17 anos
+// avisar para voltar quando fizer 18 anos
+
+// if (!(idade >= 18) || idade === 17) {
+//     console.log('Entrada não permitida!')
+// } else {
+//     console.log('Deixar entrar')
+// }
+
+console.log(2 * 2) // 4
+console.log(2 / 2) // 1
+console.log(2 % 1.5) // 0.5
+console.log(2 + 2) // 4
+console.log(2 - 2) // 0
